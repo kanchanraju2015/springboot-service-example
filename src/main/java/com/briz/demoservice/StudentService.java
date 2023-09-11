1b8ff13ecf52ae279e00dfc5591e1bcdc9214f3a
Kanchan Raju <kanchanraju2015@gmail.com>
2023-09-11 18:02:59 +0530

    Update StudentService.java

--- src/main/java/com/briz/demoservice/StudentService.java
+++ src/main/java/com/briz/demoservice/StudentService.java
@@ -6,5 +6,8 @@ public interface StudentService
 {
 public Employee addStudent(Student student);
 public List<Student> listStudent();
+  List<Student> getByName(String name);
+  List<Student> getByAge(int age);
+  List<Student> getByAddress(String address);
 
 }
