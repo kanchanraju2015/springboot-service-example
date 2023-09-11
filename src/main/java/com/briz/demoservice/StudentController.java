f78e6c96ede97296fab7b5c92af7e616ae078e6b
kanchan raju <kanchanraju2015@gmail.com>
2023-09-11 19:39:45 +0530

    new commit byname added

--- src/main/java/com/briz/demoservice/StudentController.java
+++ src/main/java/com/briz/demoservice/StudentController.java
@@ -3,6 +3,7 @@ package com.briz.demoservice;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,5 +27,10 @@ public String test()
 public List<Student> allstudents()
 {
 	return sserv.listStudent();// calling the service method 
+}
+@RequestMapping("/by/{name}")
+public List<Student> byname(@PathVariable String name)
+{
+	return sserv.getByName(name);
 }
 }
